4b984bdeccc426339abf64b839dcf8814db9dac2
Marco Dell'Anna <m.dellanna@reply.it>
2023-11-10 11:56:59 +0100

    naked single fix
    naked single fix

--- js/techniques/impl/naked_single.js
+++ js/techniques/impl/naked_single.js
@@ -3,10 +3,12 @@ const naked_single_impl = () => {
 
     // LOOP THROUGH ALL UNSOLVED CELLS
     for (let i = 0; i < 9; i++)
-        for (let j = 0; j < 9; j++){
+        for (let j = 0; j < 9; j++) {
+            const cand_list = get_candidate_list_by_index(i, j);
             // FIND UNSOLVED CELLS WITH ONLY ONE CANDIDATE
-            if (!is_solved_by_index(i, j) && get_candidate_list_by_index(i, j).length == 1)
-                naked.push({value: cand_list[0].textContent, naked: cells[i][j]});
+            if (!is_solved_by_index(i, j) && cand_list.length == 1)
+                naked.push({ value: cand_list[0].textContent, naked: cells[i][j] });
         }
+
     return naked;
 }
